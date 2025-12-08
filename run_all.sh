b52ef7c6c6ba695ab7eccd4c34d05e023453db60
Nawfal Adil <144803416+nawfaladil@users.noreply.github.com>
2025-12-08 15:01:02 +0100

    Delete run_all.sh

--- run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Start FastAPI backend
-uvicorn main_tool.core.api.app:app --reload &
-BACKEND_PID=$!
-# Start Streamlit frontend
-streamlit run main_tool/core/front/front.py &
-FRONTEND_PID=$!
-# Wait for both to finish
-wait $BACKEND_PID $FRONTEND_PID
